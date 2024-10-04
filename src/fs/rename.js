0683532502712050eb13626eb9f2b1de4661f09f
Angelina Bezik <angelina1337bz@gmail.com>
2024-10-04 18:41:05 +0300

    feat: implement delete and rename functions

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const oldFileName = join(__dirname, 'files', 'wrongFilename.txt');
+    const newFileName = join(__dirname, 'files', 'properFilename.md');
+    try {
+        await fs.promises.access(oldFileName);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await fs.promises.access(newFileName);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+    }
+
+    await fs.promises.rename(oldFileName, newFileName);
 };
 
 await rename();
