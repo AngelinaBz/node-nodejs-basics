45f998d85cd5c0feae8bb7b9cd48a77960ba3d8b
Angelina Bezik <angelina1337bz@gmail.com>
2024-10-04 20:25:59 +0300

    feat: implement stream functions

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const readFile = join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(readFile, 'utf-8');
+    readStream.on('data', (chunck) => stdout.write(chunck + "\n"))
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const writeFile = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(writeFile, 'utf-8');
+    stdout.write('Write something: \n');
+    stdin.on('data', (chunk) => {
+        writeStream.write(chunk + '\n');
+    });
+    stdin.on('end', () => {
+        writeStream.end();
+    });
 };
 
 await write();
