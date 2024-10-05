55709bade2db542362531d9e3451629e49a3cd36
Angelina Bezik <angelina1337bz@gmail.com>
2024-10-05 21:45:51 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256');
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    const file = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const fileStream = fs.createReadStream(file);
+
+    try {
+		await fs.promises.access(file);
+
+		fileStream.on('data', chunk => {
+			hash.update(chunk);
+		});
+		fileStream.on('end', () => {
+			console.log(hash.digest('hex'));
+		});
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			throw new Error(`Failed`);
+		}
+	}
 };
 
 await calculateHash();
